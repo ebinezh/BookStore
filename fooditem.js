f172a49a25e0c96e10fc1b21f08dfe8883e4c05c
ebinezh <ebinezhmohan.eee2020@citchennai.net>
2023-06-08 22:13:09 +0530

    BookManage

---
+++ fooditem.js
@@ -0,0 +1,32 @@
+const foodItem= [
+ 
+{
+    id: 10,
+    name: 'Nineteen Lives',
+    category : 'books',
+    rating : 4.3,
+    price: 11,
+    img: 'images/book1/nine.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Nineteen Vol 2',
+    category : 'books',
+    rating : 4.3,
+    price: 17,
+    img: 'images/book1/nine.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Nineteen Live Vol3',
+    category : 'books',
+    rating : 4.3,
+    price: 20,
+    img: 'images/book1/nine.jpeg',
+    quantity: 1
+}
+]
+
+export {foodItem};
---
+++ index.js
@@ -0,0 +1,89 @@
+import {foodItem} from './fooditem.js'
+
+function displayItems(){
+
+    var books=  document.getElementById('books');
+    const booksData= foodItem.filter((item)=>item.category=='books');
+
+    booksData.map(item=>{
+        var itemCard= document.createElement('div');
+        itemCard.setAttribute('id','item-card')
+
+        var cardTop= document.createElement('div');
+        cardTop.setAttribute('id','card-top');
+
+        var star= document.createElement('i');
+        star.setAttribute('class','fa fa-star');
+        star.setAttribute('id','rating');
+        star.innerText= ' ' + item.rating;
+
+        var heart= document.createElement('i');
+        heart.setAttribute('class','fa fa-heart-o add-to-cart');
+        heart.setAttribute('id',item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemName= document.createElement('p');
+        itemName.setAttribute('id','item-name');
+        itemName.innerText= item.name;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+        
+        books.appendChild(itemCard)
+
+    })
+
+
+    vegetableData.map(item=>{
+        
+        var img= document.createElement('img');
+        img.src=item.img;
+
+        var itemPrice= document.createElement('p');
+        itemPrice.setAttribute('id','item-price');
+        itemPrice.innerText= 'Price : $ ' + item.price;
+
+
+        itemCard.appendChild(img);
+        
+    
+    })
+8    
+}
+displayItems();
+
+
+function cartItems(){
+    var tableBody=  document.getElementById('table-body');
+    tableBody.innerHTML= '';
+
+    cartData.map(item=> {
+        var tableRow= document.createElement('tr');
+        
+        var rowData1= document.createElement('td');
+        var img= document.createElement('img');
+        img.src= item.img;
+        rowData1.appendChild(img);
+
+      
+    })
+    document.querySelectorAll('.increase-item').forEach(item=>{
+        item.addEventListener('click',incrementItem)
+    })
+
+    document.querySelectorAll('.decrease-item').forEach(item=>{
+        item.addEventListener('click',decrementItem)
+    })
+}
